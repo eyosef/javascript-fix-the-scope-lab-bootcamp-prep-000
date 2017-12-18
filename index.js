acc809f3a54cc9c95b7d483fff955ae23d28ac93
Eden Mesfun <emesfun@gmail.com>
2017-12-18 20:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
-  console.log
+  console.log('cat')
 }
 
 function add2(n) {
