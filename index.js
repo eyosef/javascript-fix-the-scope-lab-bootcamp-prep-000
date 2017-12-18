80db42fde020efb8d189b6bc93ae72c13637e5a9
Eden Mesfun <emesfun@gmail.com>
2017-12-18 20:17:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function('FUNKY!') {
-    return "FUNKY!"
-  }
+  return function('FUNKY!') 
+  
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
